22885836790344b9e8ac6df64e58e994358fbf37
maciejjablonsky <maciekjablonsky@gmail.com>
2020-02-22 03:09:02 +0100

    Added destructor of config_parser

--- src/config_parser/config_parser.c
+++ src/config_parser/config_parser.c
@@ -4,7 +4,6 @@
 
 #include "config_parser.h"
 #include "../object.h"
-#include "files.h"
 #include "../memory_handling.h"
 #include <libxml/parser.h>
 
@@ -12,13 +11,14 @@ typedef enum {
     CONFIG_PARSER_NO_MEMORY, CONFIG_PARSER_XML_NOT_PARSED_FAIL, CONFIG_PARSER_EMPTY_XML_FAIL
 } config_parser_error_code_t;
 
-static config_parser_t * delete_failed_config_parser(config_parser_t * this, config_parser_error_code_t error_code);
+static config_parser_t *delete_failed_config_parser(config_parser_t *this, config_parser_error_code_t error_code);
 
-static bool is_parsed(xmlDoc * doc);
-static bool is_not_parsed(xmlDoc* doc);
+static bool is_parsed(xmlDoc *doc);
+
+static bool is_not_parsed(xmlDoc *doc);
 
 config_parser_t *new_config_parser(const char *xml_filename) {
-    config_parser_t * this = new_object(sizeof(config_parser_t));
+    config_parser_t *this = new_object(sizeof(config_parser_t));
     if (is_not_created(this)) {
         return delete_failed_config_parser(this, CONFIG_PARSER_NO_MEMORY);
     }
@@ -34,16 +34,28 @@ config_parser_t *new_config_parser(const char *xml_filename) {
     return this;
 }
 
-static config_parser_t * delete_failed_config_parser(config_parser_t * this, config_parser_error_code_t error_code) {
-    switch(error_code) {
+config_parser_t *delete_config_parser(config_parser_t *this) {
+    if (is_not_created(this)) { return NULL; }
+    if (is_created(this->doc)) {
+        xmlFreeDoc(this->doc);
+        this->doc = NULL;
+    }
+    if (is_created(this->root)) {
+        xmlFreeNode(this->root);
+    }
+    return delete_object(this);
+}
+
+static config_parser_t *delete_failed_config_parser(config_parser_t *this, config_parser_error_code_t error_code) {
+    switch (error_code) {
         case CONFIG_PARSER_NO_MEMORY:
             MEMORY_NOT_ALLOCATED_MESSAGE();
             break;
         case CONFIG_PARSER_XML_NOT_PARSED_FAIL:
-            EXPLICIT_ERROR_MESSAGE("Failed to parse xml config file.");
+        EXPLICIT_ERROR_MESSAGE("Failed to parse xml config file.");
             break;
         case CONFIG_PARSER_EMPTY_XML_FAIL:
-            EXPLICIT_ERROR_MESSAGE("Given configuration file is empty.");
+        EXPLICIT_ERROR_MESSAGE("Given configuration file is empty.");
             break;
         default:
             break;
