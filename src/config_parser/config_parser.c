e82434eab691c1f7a905dd3be32374cb4a5a2c73
maciejjablonsky <maciekjablonsky@gmail.com>
2020-02-22 03:05:04 +0100

    Added config_parser that stores xml_tree

--- src/config_parser/config_parser.c
+++ src/config_parser/config_parser.c
@@ -3,4 +3,62 @@
 //
 
 #include "config_parser.h"
+#include "../object.h"
+#include "files.h"
+#include "../memory_handling.h"
+#include <libxml/parser.h>
+
+typedef enum {
+    CONFIG_PARSER_NO_MEMORY, CONFIG_PARSER_XML_NOT_PARSED_FAIL, CONFIG_PARSER_EMPTY_XML_FAIL
+} config_parser_error_code_t;
+
+static config_parser_t * delete_failed_config_parser(config_parser_t * this, config_parser_error_code_t error_code);
+
+static bool is_parsed(xmlDoc * doc);
+static bool is_not_parsed(xmlDoc* doc);
+
+config_parser_t *new_config_parser(const char *xml_filename) {
+    config_parser_t * this = new_object(sizeof(config_parser_t));
+    if (is_not_created(this)) {
+        return delete_failed_config_parser(this, CONFIG_PARSER_NO_MEMORY);
+    }
+    this->doc = xmlParseFile(xml_filename);
+    if (is_not_parsed(this->doc)) {
+        EXPLICIT_ERROR_MESSAGE("Config file: %s", xml_filename);
+        return delete_failed_config_parser(this, CONFIG_PARSER_XML_NOT_PARSED_FAIL);
+    }
+    this->root = xmlDocGetRootElement(this->doc);
+    if (this->root == NULL) {
+        return delete_failed_config_parser(this, CONFIG_PARSER_EMPTY_XML_FAIL);
+    }
+    return this;
+}
+
+static config_parser_t * delete_failed_config_parser(config_parser_t * this, config_parser_error_code_t error_code) {
+    switch(error_code) {
+        case CONFIG_PARSER_NO_MEMORY:
+            MEMORY_NOT_ALLOCATED_MESSAGE();
+            break;
+        case CONFIG_PARSER_XML_NOT_PARSED_FAIL:
+            EXPLICIT_ERROR_MESSAGE("Failed to parse xml config file.");
+            break;
+        case CONFIG_PARSER_EMPTY_XML_FAIL:
+            EXPLICIT_ERROR_MESSAGE("Given configuration file is empty.");
+            break;
+        default:
+            break;
+    }
+    return delete_config_parser(this);
+}
+
+bool is_parsed(xmlDoc *doc) {
+    return doc != NULL;
+}
+
+bool is_not_parsed(xmlDoc *doc) {
+    return !is_parsed(doc);
+}
+
+
+
 
--- src/config_parser/config_parser.h
+++ src/config_parser/config_parser.h
@@ -1,6 +1,15 @@
 #ifndef AQUARIUM_CONFIG_PARSER_H
 #define AQUARIUM_CONFIG_PARSER_H
+#include <libxml/tree.h>
 
+typedef struct {
+    xmlDoc * doc;
+    xmlNode * root;
+} config_parser_t;
+#define CONFIG_PARSER_T_NAME "config_parser_t"
+
+config_parser_t * new_config_parser(const char * xml_filename);
+config_parser_t * delete_config_parser(config_parser_t * this);
 
 
 #endif //AQUARIUM_CONFIG_PARSER_H
